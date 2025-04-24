2f133e476c4dfc91b9b6ee59415a4c4e22d62e0c
Vitaly Bokser <vbokser@gmail.com>
2025-04-23 21:43:46 -0500

    ok the app is faster now to draw the grey scale image with the bands need to fix max bands and histogram on off

--- Perfect Green Screen/PGSViewController.swift
+++ Perfect Green Screen/PGSViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import AVFoundation
 import DGCharts
+import Accelerate
 
 class PGSViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
@@ -247,110 +248,27 @@ class PGSViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferD
     
     
     func captureOutput(_ captureOutput: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-
-        if !self.getHistogramData { //need this to sync data up so histogram is not fucked
+        if !self.getHistogramData {
             return
         }
 
-        let start = CFAbsoluteTimeGetCurrent()
-
         let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
         
-        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
-        
-        let width = CVPixelBufferGetWidth(imageBuffer)
-        let height = CVPixelBufferGetHeight(imageBuffer)
-        let bitsPerComponent = 8
-        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
-        
-        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)!
-        let byteBuffer = baseAddress.assumingMemoryBound(to: UInt8.self)
-        
-//        self.getHistogramData = true
-        if self.getHistogramData { //every 15 frames approx
-            for i in 0..<numberValueslsInByte {
-                histogramData[i] = 0
-            }
-//            histogramData = Array<Int>(repeating: 0, count: numberValueslsInByte)
-        }
-        
-        self.total_pixel_count = Double(height * width)
-        
-        for j in 0..<height {
-            for i in 0..<width {
-                let index = (j * width + i) * 4
-                
-                let b = byteBuffer[index]
-                let g = byteBuffer[index+1]
-                let r = byteBuffer[index+2]
-                //let a = byteBuffer[index+3]
-                
-//                let bwpixel = (b + g + r) / 3
-                let red: Float = 0.3 * Float(r)
-                let blue: Float = 0.11 * Float(b)
-                let green: Float = 0.59 * Float(g)
-                let linearIntensityD: Float = red + green + blue //(0.3 * r) + (0.59 * g) + (0.11 * b)
-                var linearIntensity = Int(linearIntensityD)
-                
-                if self.getHistogramData {
-                    if (linearIntensity < 0 || linearIntensity > 255) {
-                        print("linear intensity is incorrect = \(linearIntensity)")
-                    }
-                    else {
-                        histogramData[linearIntensity] = histogramData[linearIntensity] + 1
-                    }
-                }
-
-                linearIntensity = getIntensity(intensity: linearIntensity)
-                
-                byteBuffer[index] = UInt8(linearIntensity)
-                byteBuffer[index+1] = UInt8(linearIntensity)
-                byteBuffer[index+2] = UInt8(linearIntensity)
-                
-//                if r > UInt8(128) && g < UInt8(128) {
-//                    byteBuffer[index] = UInt8(255)
-//                    byteBuffer[index+1] = UInt8(0)
-//                    byteBuffer[index+2] = UInt8(0)
-//                } else {
-//                    byteBuffer[index] = g
-//                    byteBuffer[index+1] = r
-//                    byteBuffer[index+2] = b
-//                }
-            }
-        }
-        
-        if (self.doUpdateCharts == false) {
-            for i in 0..<numberValueslsInByte {
-                self.newHistogramData[i] = histogramData[i]
-                histogramData[i] = 0
-            }
-            self.doUpdateCharts = true
-        }
-        self.getHistogramData = false
-//        if(self.timingSetter) {
-//            self.timingSetter = false
-//        }
-//        if self.getHistogramData { //dont get more histogram data until timer hits
-//            self.getHistogramData = false
-//            timer = Timer.scheduledTimer(timeInterval: 0.25, target: self, selector: #selector(self.updateChart), userInfo: nil, repeats: false)
-//        }
-        
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let bitmapInfo = CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
-        let newContext = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
-        if let context = newContext {
-            let cameraFrame = context.makeImage()
+        // Process with vImage
+        if let processedImage = processImageWithVImage(pixelBuffer: imageBuffer, numberOfBands: self.numberOfBands) {
             DispatchQueue.main.async {
-                self.processedView.clipsToBounds = true
-                self.processedView.contentMode = .scaleAspectFit
-                self.processedView.image = UIImage(cgImage: cameraFrame!)
+                self.processedView.image = processedImage
             }
         }
         
-        CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
+        // Update histogram data separately to avoid slowing down image processing
+        if self.getHistogramData {
+            DispatchQueue.global(qos: .utility).async {
+                self.updateHistogramFromBuffer(imageBuffer)
+            }
+        }
         
-//        let diff = CFAbsoluteTimeGetCurrent() - start
-//        print("Took \(diff) seconds")
+        self.getHistogramData = false
     }
     
     func getIntensity(intensity: Int) -> Int {
@@ -585,26 +503,105 @@ extension PGSViewController {
 //        setupCurrentSliderBandsValue()
     }
     
-//    func setupCurrentSliderBandsValue() {
-//        var currentNumBands = 2
-//
-//        //check if key exists if not set default value
-//        if !userDefaultsExist(key: CURRENT_NUMBER_OF_BANDS_STRING) {
-//            //set in the middle
-//            currentNumBands = Int(DEFAULT_MAX_NUMBER_OF_BANDS_INT / 2)
-//            UserDefaults.standard.set(currentNumBands, forKey: CURRENT_NUMBER_OF_BANDS_STRING)
-//        }
-//
-//        currentNumBands = UserDefaults.standard.integer(forKey: CURRENT_NUMBER_OF_BANDS_STRING)
-//        let maxNumberOfBands = self.bandsSlider.maximumValue
-//        if (currentNumBands <= Int(maxNumberOfBands)) {
-//            self.bandsSlider.value = Float(currentNumBands)
-//        }
-//        else { //current value is greater than max value
-//            self.bandsSlider.value = Float(maxNumberOfBands)
-//            UserDefaults.standard.set(maxNumberOfBands, forKey: CURRENT_NUMBER_OF_BANDS_STRING)
-//        }
-//    }
+    func processImageWithVImage(pixelBuffer: CVPixelBuffer, numberOfBands: Int) -> UIImage? {
+        let width = CVPixelBufferGetWidth(pixelBuffer)
+        let height = CVPixelBufferGetHeight(pixelBuffer)
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
+        let bandSize = 256 / numberOfBands
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer, .readOnly)
+        defer { CVPixelBufferUnlockBaseAddress(pixelBuffer, .readOnly) }
+        
+        // Create output buffer for the processed image
+        let context = CGContext(
+            data: nil,
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: bytesPerRow,
+            space: CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+        )
+        
+        guard let context = context else { return nil }
+        
+        // Get pointers to input and output data
+        let inputData = CVPixelBufferGetBaseAddress(pixelBuffer)!.assumingMemoryBound(to: UInt8.self)
+        let outputData = context.data!.assumingMemoryBound(to: UInt8.self)
+        
+        // Process pixels - apply grayscale and banding
+        for y in 0..<height {
+            for x in 0..<width {
+                let pixelIndex = y * bytesPerRow + x * 4
+                
+                // Get RGB components (BGRA format in buffer)
+                let blue = Float(inputData[pixelIndex])
+                let green = Float(inputData[pixelIndex + 1])
+                let red = Float(inputData[pixelIndex + 2])
+                
+                // Convert to grayscale
+                let gray = 0.3 * red + 0.59 * green + 0.11 * blue
+                
+                // Apply banding
+                let band = Int(gray) / bandSize
+                let bandedValue = UInt8(band * bandSize)
+                
+                // Write grayscale to all three RGB channels
+                outputData[pixelIndex] = bandedValue     // B
+                outputData[pixelIndex + 1] = bandedValue // G
+                outputData[pixelIndex + 2] = bandedValue // R
+                outputData[pixelIndex + 3] = 255         // A
+            }
+        }
+        
+        // Create image
+        guard let cgImage = context.makeImage() else { return nil }
+        return UIImage(cgImage: cgImage)
+    }
 
+    func updateHistogramFromBuffer(_ imageBuffer: CVPixelBuffer) {
+        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
+        defer { CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0))) }
+        
+        let width = CVPixelBufferGetWidth(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)!
+        let byteBuffer = baseAddress.assumingMemoryBound(to: UInt8.self)
+        
+        // Reset histogram data
+        for i in 0..<numberValueslsInByte {
+            histogramData[i] = 0
+        }
+        
+        // Sample fewer pixels for performance - maybe every 4th pixel
+        for j in stride(from: 0, to: height, by: 4) {
+            for i in stride(from: 0, to: width, by: 4) {
+                let index = (j * bytesPerRow) + (i * 4)
+                
+                let b = byteBuffer[index]
+                let g = byteBuffer[index+1]
+                let r = byteBuffer[index+2]
+                
+                let red: Float = 0.3 * Float(r)
+                let blue: Float = 0.11 * Float(b)
+                let green: Float = 0.59 * Float(g)
+                let linearIntensity = Int(red + green + blue)
+                
+                if linearIntensity >= 0 && linearIntensity < 256 {
+                    histogramData[linearIntensity] += 1
+                }
+            }
+        }
+        
+        // Update histogram data for chart
+        for i in 0..<numberValueslsInByte {
+            self.newHistogramData[i] = histogramData[i]
+        }
+        
+        self.doUpdateCharts = true
+    }
 }
+
+
 
